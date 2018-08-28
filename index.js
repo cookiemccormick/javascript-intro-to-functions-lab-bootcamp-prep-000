b1a47bfb12400906778d67f1888b514068062f61
Alisa McCormick <muntananuchat@gmail.com>
2018-08-28 15:23:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  return '$I can't hear you!"
+  return '${I can't hear you!}'
   return "YES INDEED!"
   if string.mixedCase("I love you, too.")
   return
