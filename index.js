a69beb29d7db1e82a78c400372de41084c80e829
Alisa McCormick <muntananuchat@gmail.com>
2018-08-28 15:52:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function sayHiToGrandma(string) {
   if string === string.toLowerCase() {
     return "I can\'t hear you!";
   } else if string = stting.toUpperCase() {
-    return "YES INDEED!"
+    return "YES INDEED!";
   } else ("I love you, Grandma.") {
     return "I love you, too."
   }
